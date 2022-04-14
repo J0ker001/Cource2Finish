182a2cedccbe224608098162fe00e3a0b2cb4b23
Антон <antonL1790@yandex.ru>
2022-04-14 22:51:21 +0300

    Initial commit

---
+++ src/main/java/pro/sky/cource2finish/Cource2FinishApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.cource2finish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cource2FinishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cource2FinishApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/cource2finish/Cource2FinishApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.cource2finish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cource2FinishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
